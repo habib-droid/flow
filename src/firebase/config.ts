3ba7834818d21a244c4d9d4a165e66a38656bd41
habib-droid <habib@sentrixmedia.com>
2026-04-06 01:19:59 +0500

    add firbase api key to .env

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-9807865423-c7d87",
   "appId": "1:784178809587:web:bcc471d846cb3b217e0586",
-  "apiKey": "AIzaSyB0-pVIfDUIvEBkqJc_FEiSI0aH8Qg3nrE",
+  "apiKey": process.env.FIRBASE_API_KEY,
   "authDomain": "studio-9807865423-c7d87.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "784178809587"
