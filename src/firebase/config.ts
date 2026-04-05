f7c3f9746d7ce995175473d6c220b6d9faed9af9
habib-droid <habib@sentrixmedia.com>
2026-04-06 01:23:39 +0500

    change api key key

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,7 @@
 export const firebaseConfig = {
   "projectId": "studio-9807865423-c7d87",
   "appId": "1:784178809587:web:bcc471d846cb3b217e0586",
-  "apiKey": process.env.FIRBASE_API_KEY,
+  "apiKey": process.env.NEXT_PUBLIC_FIRBASE_API_KEY,
   "authDomain": "studio-9807865423-c7d87.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "784178809587"
